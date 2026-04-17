282cd1af6d7135daf318b1d916794cee797d2e39
Faisal Hanif <hatilas1994@gmail.com>
2026-04-17 16:52:07 +0500

    made a fake prompt template

--- learning.py
+++ learning.py
@@ -17,3 +17,19 @@ class FakeLLM:
 llm = FakeLLM()
 
 print(llm.predict('What is the capital of Pakistan'))
+
+
+class FakePromptTemplate:
+
+    def __init__(self, template, input_variables):
+        self.template = template
+        self.input_variables = input_variables
+
+    def format(self, input_dict):
+        return self.template.format(**input_dict)
+template = FakePromptTemplate(
+    template='Write a poem about {topic}',
+    input_variables=['topic']
+)
+
+print(template.format({'topic':'Pakistan'}))
