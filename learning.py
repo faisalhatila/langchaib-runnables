5cd0ff1287e790eea8d7496b2be893a4aa65c3c5
Faisal Hanif <hatilas1994@gmail.com>
2026-04-17 17:04:54 +0500

    made a chain but it's not standardized. So next we move on standards

--- learning.py
+++ learning.py
@@ -22,16 +22,35 @@ class FakePromptTemplate:
 
     def format(self, input_dict):
         return self.template.format(**input_dict)
+
+
+class FakeLLMChain:
+    def __init__(self,llm, prompt):
+        self.llm = llm
+        self.prompt = prompt
+    
+    def run(self, input_dict):
+        final_prompt = self.prompt.format(input_dict)
+        result = self.llm.predict(final_prompt)
+
+        return result['response']
+    
+
 template = FakePromptTemplate(
     template='Write a {length} poem about {topic}',
     input_variables=['length','topic']
 )
 
-prompt = template.format({'length':'short','topic':'Pakistan'})
-
 llm = FakeLLM()
 
-result = llm.predict(prompt)
+chain = FakeLLMChain(llm, template)
+
+result = chain.run({'length':'short','topic':'Pakistan'})
+
+# prompt = template.format({'length':'short','topic':'Pakistan'})
+
+
+# result = llm.predict(prompt)
 
 # print(llm.predict('What is the capital of Pakistan'))
 print(result)
