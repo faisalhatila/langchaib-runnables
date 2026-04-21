cb5302ee621b89d350e615fa6f3213a8e7764f46
Faisal Hanif <hatilas1994@gmail.com>
2026-04-21 19:56:52 +0500

    Add runnable sequence implementation for joke generation and explanation
    - Introduced runnable_sequence.py to create a chain for generating jokes about a given topic and explaining them.
    - Utilized ChatOpenAI for joke generation and StrOutputParser for output processing.
    - Implemented two PromptTemplates for joke creation and explanation, enhancing the functionality of the RunnableSequence.

---
+++ primitive_runnables/runnable_sequence.py
@@ -0,0 +1,28 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from dotenv import load_dotenv
+from langchain_classic.schema.runnable import RunnableSequence
+
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+result = chain.invoke({'topic':'AI'})
+
+print(result)
