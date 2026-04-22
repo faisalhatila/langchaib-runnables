e0ba3b2ebf8093fee693c352f734ff2b9eb22cef
Faisal Hanif <hatilas1994@gmail.com>
2026-04-23 03:15:01 +0500

    Enhance runnable_lambda.py with LLM integration for joke generation
    - Expanded runnable_lambda.py to include a complete LLM chain for joke generation using HuggingFace's Llama model.
    - Introduced new PromptTemplates for joke creation and word counting.
    - Implemented RunnableSequence and RunnableParallel to streamline the processing of jokes and word counts.
    - Updated the word counting function and added example invocation to demonstrate the new functionality.

--- primitive_runnables/runnable_lambda.py
+++ primitive_runnables/runnable_lambda.py
@@ -1,12 +1,61 @@
-# Runnable lambda example: Convert any function into runnable
+# # Runnable lambda example: Convert any function into runnable
 
-from langchain_classic.schema.runnable import RunnableLambda
+# from langchain_classic.schema.runnable import RunnableLambda
 
-def word_counter(text):
-    return len(text.split(' '))
+# def word_counter(text):
+#     return len(text.split(' '))
 
-runnable_word_counter = RunnableLambda(word_counter)
+# runnable_word_counter = RunnableLambda(word_counter)
 
-count = runnable_word_counter.invoke('Hi there, how are you?')
+# count = runnable_word_counter.invoke('Hi there, how are you?')
 
-print(count)
+# print(count) 
+
+
+from langchain_openai import ChatOpenAI
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from dotenv import load_dotenv
+from langchain_classic.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+ 
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatHuggingFace(llm = llm)
+
+parser = StrOutputParser()
+
+def word_count(text):
+    # return text.split()
+    return len(text.split())
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke':RunnablePassthrough(),
+    'word_count':RunnableLambda(word_count)
+})
+
+final_Chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_Chain.invoke({'topic':'India'})
+
+formatted_result = """{} \n word count - {} """.format(result['joke'],result['word_count'])
+
+print(formatted_result)
