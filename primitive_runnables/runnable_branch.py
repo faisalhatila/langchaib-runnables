34c664ac30e7143671d56a25e64d4d544c3c41ca
Faisal Hanif <hatilas1994@gmail.com>
2026-04-23 17:42:25 +0500

    Add runnable_branch implementation for report generation and summarization
    - Introduced runnable_branch.py to create a chain for generating detailed reports and summarizing text based on a given topic.
    - Integrated HuggingFace's Llama model for text generation and included PromptTemplates for report writing and summarization.
    - Implemented RunnableSequence and RunnableBranch to manage the flow of report generation and conditional summarization based on word count.
    - Added example invocation to demonstrate the functionality of the new runnable component.

---
+++ primitive_runnables/runnable_branch.py
@@ -0,0 +1,45 @@
+from langchain_openai import ChatOpenAI
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from dotenv import load_dotenv
+from langchain_classic.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda, RunnableBranch
+
+
+load_dotenv()
+
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+# model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+report_gen_chain = RunnableSequence(prompt1, model, parser)
+
+branch_chain = RunnableBranch(
+    (lambda x:len(x.split())>500, RunnableSequence(prompt2, model, parser)),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+result = final_chain.invoke({'topic':'USA vs IRAN war'})
+
+formatted_result = """{} \n word count - {} """.format(result,len(result.split()))
+
+print(formatted_result)
